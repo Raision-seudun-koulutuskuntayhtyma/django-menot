ede12d6ded3a75455f197466076679b1f5b6fb58
Tuomas Suutari <tuomas@nepnep.net>
2023-11-06 15:59:59 +0200

    Lisää muutama sivu (accounts ja documents)

--- moneyflow/urls.py
+++ moneyflow/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.frontpage, name="index"),
+    path("tilit/", views.accounts, name="accounts"),
+    path("dokumentit/", views.documents, name="documents"),
 ]
--- moneyflow/views.py
+++ moneyflow/views.py
@@ -2,12 +2,25 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from .models import Account
+from .models import Account, Document
 
 
 @login_required
 def frontpage(request):
+    return render(request, "moneyflow/index.html")
+
+
+@login_required
+def accounts(request):
     context = {
         "accounts": Account.objects.filter(owner=request.user),
     }
-    return render(request, "moneyflow/index.html", context)
+    return render(request, "moneyflow/accounts.html", context)
+
+
+@login_required
+def documents(request):
+    context = {
+        "documents": Document.objects.filter(owner=request.user),
+    }
+    return render(request, "moneyflow/documents.html", context)
