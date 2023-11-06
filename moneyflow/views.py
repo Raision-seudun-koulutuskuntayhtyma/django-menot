4163455806cbf5041ee61e96732aa5766226c39a
Tuomas Suutari <tuomas@nepnep.net>
2023-11-06 15:12:39 +0200

    Näytä käyttäjän tilit etusivulla

--- moneyflow/views.py
+++ moneyflow/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
+from .models import Account
 
 
+@login_required
 def frontpage(request):
-    return render(request, "moneyflow/index.html")
+    context = {
+        "accounts": Account.objects.filter(owner=request.user),
+    }
+    return render(request, "moneyflow/index.html", context)
