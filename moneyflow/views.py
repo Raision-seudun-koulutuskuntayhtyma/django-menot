dbca07bbf9837641cce2522862c4714b375339f2
Tuomas Suutari <tuomas@nepnep.net>
2023-11-13 15:39:39 +0200

    views.AccountDetail

--- moneyflow/views.py
+++ moneyflow/views.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect, render
-from django.urls import reverse
-from django.views.generic import ListView
+from django.shortcuts import render
+from django.views.generic import DetailView, ListView, View
 
 from .models import Account, Document
 
@@ -12,11 +11,18 @@ def frontpage(request):
     return render(request, "moneyflow/index.html")
 
 
-class AccountsList(LoginRequiredMixin, ListView):
+class AccountView(View):
     model = Account
 
     def get_queryset(self):
         return super().get_queryset().filter(owner=self.request.user)
+
+
+class AccountsList(LoginRequiredMixin, AccountView, ListView):
+    pass
+
+class AccountDetail(LoginRequiredMixin, AccountView, DetailView):
+    pass
 
 
 @login_required
